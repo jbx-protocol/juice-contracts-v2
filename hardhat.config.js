2f6ba12e4b7dbd17a1de20d36dbbbfd0aa26930d
odd-amphora <odd_amphora@protonmail.com>
2021-09-15 17:52:37 -0400

    Tests kind of working cleaning up config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,102 @@
+const { utils } = require("ethers");
+const fs = require("fs");
+const chalk = require("chalk");
+const dotenv = require("dotenv");
+
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+// TODO(odd-amphora): deploy
+// require("./tasks/deploy");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+dotenv.config();
 
-  for (const account of accounts) {
-    console.log(account.address);
+//
+// Select the network you want to deploy to here:
+//
+const defaultNetwork = "localhost";
+
+function mnemonic() {
+  try {
+    return fs.readFileSync("./mnemonic.txt").toString().trim();
+  } catch (e) {
+    if (defaultNetwork !== "localhost") {
+      console.log(
+        "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
+      );
+    }
   }
-});
+  return "";
+}
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const infuraId = process.env.INFURA_API_KEY;
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork,
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/" + infuraId,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/" + infuraId,
+      gasPrice: 50000000000,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/" + infuraId,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/" + infuraId,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + infuraId,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    xdai: {
+      url: "https://dai.poa.network",
+      gasPrice: 1000000000,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+  },
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        // https://docs.soliditylang.org/en/v0.6.3/using-the-compiler.html
+        runs: 10000,
+      },
+    },
+  },
+  mocha: {
+    bail: true,
+    timeout: 6000,
+  },
+  gasReporter: {
+    currency: "USD",
+    // gasPrice: 21,
+    enabled: !!process.env.REPORT_GAS,
+    showTimeSpent: true,
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
 };
