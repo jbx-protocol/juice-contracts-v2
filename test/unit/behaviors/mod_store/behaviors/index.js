6642ac7425a86ee460408d3c771f0c01b93d7983
odd-amphora <odd_amphora@protonmail.com>
2021-09-15 17:42:23 -0400

    Add v1 tests

---
+++ test/unit/behaviors/mod_store/behaviors/index.js
@@ -0,0 +1,7 @@
+const setPayoutMods = require("./set_payment_mods");
+const setTicketMods = require("./set_ticket_mods");
+
+module.exports = {
+  setPayoutMods,
+  setTicketMods
+};
---
+++ test/unit/behaviors/prices/behaviors/index.js
@@ -0,0 +1,7 @@
+const getETHPriceFor = require("./get_eth_price_for");
+const addFeed = require("./add_feed");
+
+module.exports = {
+  getETHPriceFor,
+  addFeed
+};
