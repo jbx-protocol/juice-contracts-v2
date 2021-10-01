4efd498eaf43a49f7280d728a76e2ce36531f138
odd-amphora <odd_amphora@protonmail.com>
2021-10-01 18:05:33 -0400

    Messy set URI tests

--- test/unit/v2/jb_projects/index.js
+++ test/unit/v2/jb_projects/index.js
@@ -1,6 +1,7 @@
 const challengeHandle = require('./challenge_handle');
 const claimHandle = require('./claim_handle');
 const createFor = require('./create_for');
+const setUriOf = require('./set_uri_of');
 
 module.exports = function () {
   before(async function () {
@@ -9,6 +10,7 @@ module.exports = function () {
   });
 
   describe('challengeHandle(...)', challengeHandle);
-  describe('claimHandle(...)', claimHandle);
+  // describe('claimHandle(...)', claimHandle);
   describe('createFor(...)', createFor);
+  describe('setUriOf(...', setUriOf);
 };
