2d9e35d682f41a0d982158ca6f2c3f8e741e271a
mejango <mejango@protonmail.com>
2021-10-30 13:08:41 -0400

    Cleaned abstracts

--- contracts/v2/abstract/JBTerminalUtility.sol
+++ contracts/v2/abstract/JBTerminalUtility.sol
@@ -3,17 +3,24 @@ pragma solidity 0.8.6;
 
 import './../interfaces/IJBTerminalUtility.sol';
 
+/** 
+  @notice
+  Provides tools for contracts that has functionality that can only be accessed by a project's terminals.
+*/
 abstract contract JBTerminalUtility is IJBTerminalUtility {
   modifier onlyTerminal(uint256 _projectId) {
-    require(directory.isTerminalDelegateOf(_projectId, msg.sender), 'UNAUTHORIZED');
+    require(directory.isTerminalDelegateOf(_projectId, msg.sender), '0x50: UNAUTHORIZED');
     _;
   }
 
-  /// @notice The direct deposit terminals.
+  /** 
+    @notice 
+    The directory of terminals and controllers for projects.
+  */
   IJBDirectory public immutable override directory;
 
   /** 
-    @param _directory A directory of a project's current Juicebox terminal to receive payments in.
+    @param _directory A contract storing directories of terminals and controllers for each project.
   */
   constructor(IJBDirectory _directory) {
     directory = _directory;
