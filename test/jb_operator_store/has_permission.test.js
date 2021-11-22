d48129272ae3835cdedb67c75f8d527a4c0893cc
Exekias <odd_amphora@protonmail.com>
2021-11-21 21:21:17 -0500

    Has permission test done

--- test/jb_operator_store/has_permission.test.js
+++ test/jb_operator_store/has_permission.test.js
@@ -45,8 +45,92 @@ describe('JBOperatorStore::hasPermission(...)', function () {
           domain,
           permissionIndex,
         );
-      
+
       expect(hasPermission).to.be.true;
     }
   })
+
+  it('Has permission, account is not caller', async function () {
+    let caller = signers[0];
+    let operator = signers[1];
+    let domain = 1;
+    let permissionIndices = [1, 2, 3];
+
+    await jbOperatorStore
+      .connect(caller)
+      .setOperator([operator.address, domain, permissionIndices]);
+
+    for (let permissionIndex of permissionIndices) {
+      let hasPermission = await jbOperatorStore
+        .connect(caller)
+        .hasPermission(
+          operator.address,
+          /*account=*/caller.address,
+          domain,
+          permissionIndex
+        );
+
+      expect(hasPermission).to.be.true;
+    }
+  })
+
+  it(`Doesn't have permission, never set`, async function () {
+    let hasPermission = await jbOperatorStore
+      .connect(signers[0])
+      .hasPermission(
+        /*operator=*/signers[1].address,
+        /*account=*/signers[0].address,
+        /*domain=*/1,
+        /*permissionIndex=*/3,
+      );
+
+    expect(hasPermission).to.be.false;
+  })
+
+  it(`Doesn't have permission, indices differ`, async function () {
+    let caller = signers[0];
+    let operator = signers[1];
+    let domain = 1;
+
+    await jbOperatorStore
+      .connect(caller)
+      .setOperator([operator.address, domain, [/*permissionIndices=*/1, 2, 3]]);
+
+    // Test some invalid permission indices.
+    for (let permissionIndex of [4, 5, 6]) {
+      let hasPermission = await jbOperatorStore
+        .connect(caller)
+        .hasPermission(
+          operator.address,
+          /*account=*/caller.address,
+          domain,
+          permissionIndex
+        );
+
+      expect(hasPermission).to.be.false;
+    }
+  })
+
+  it(`Doesn't have permission, domain differs`, async function () {
+    let caller = signers[0];
+    let operator = signers[1];
+    let permissionIndices = [1, 2, 3]
+
+    await jbOperatorStore
+      .connect(caller)
+      .setOperator([operator.address, /*domain=*/1, permissionIndices]);
+
+    for (let permissionIndex of permissionIndices) {
+      let hasPermission = await jbOperatorStore
+        .connect(caller)
+        .hasPermission(
+          operator.address,
+          /*account=*/caller.address,
+          /*domain=*/2, // Test different domain.
+          permissionIndex
+        );
+
+      expect(hasPermission).to.be.false;
+    }
+  })  
 });
