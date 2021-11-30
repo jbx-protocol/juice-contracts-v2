1a728c60f52dda3280a001125737e8a4f84857d1
Joaquin Ayuso de Paul <joaxap@gmail.com>
2021-11-30 15:36:07 -0800

    Removed unused dependency

--- test/jb_projects/set_metadatacid_of.test.js
+++ test/jb_projects/set_metadatacid_of.test.js
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { BigNumber } from '@ethersproject/bignumber';
 
 describe('JBProjects::setMetadataCidOf(...)', function () {
 
