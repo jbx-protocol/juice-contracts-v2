760af60a8e9dbf6b206ee0c341c6842589a3a36f
drgorillamd <dr_gorilla@protonmail.com>
2021-12-08 01:04:58 +0100

    init

--- test/jb_controller/issue_token_for.js
+++ test/jb_controller/issue_token_for.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import { ethers, waffle, network } from 'hardhat';
 
+
+
 import { deployMockContract } from '@ethereum-waffle/mock-contract';
 
 import jbController from '../../artifacts/contracts/JBController.sol/JBController.json';
