ad95853e7a541a3aa7910e7445b949e730c715d6
Exekias <odd_amphora@protonmail.com>
2021-12-13 08:56:32 -0500

    Pay done

--- test/jb_project/pay.test.js
+++ test/jb_project/pay.test.js
@@ -4,12 +4,12 @@ import { expect } from 'chai';
 import { deployMockContract } from '@ethereum-waffle/mock-contract';
 
 import jbDirectory from '../../artifacts/contracts/JBDirectory.sol/JBDirectory.json';
+import jbTerminal from '../../artifacts/contracts/interfaces/IJBTerminal.sol/IJBTerminal.json'
 
 describe('JBProject::pay(...)', function () {
-  const INITIAL_PROJECT_ID = 1;
+  const PROJECT_ID = 1;
   const BENEFICIARY = ethers.Wallet.createRandom().address;
   const TOKEN = ethers.Wallet.createRandom().address;
-  const TERMINAL = ethers.Wallet.createRandom().address;
   const PREFER_CLAIMED_TOKENS = true;
   const MEMO = 'memo';
 
@@ -17,10 +17,11 @@ describe('JBProject::pay(...)', function () {
     let [deployer, ...addrs] = await ethers.getSigners();
 
     let mockJbDirectory = await deployMockContract(deployer, jbDirectory.abi);
+    let mockJbTerminal = await deployMockContract(deployer, jbTerminal.abi);
 
     let jbFakeProjectFactory = await ethers.getContractFactory('JBFakeProject');
     let jbFakeProject = await jbFakeProjectFactory.deploy(
-      INITIAL_PROJECT_ID,
+      PROJECT_ID,
       mockJbDirectory.address,
     );
 
@@ -28,16 +29,68 @@ describe('JBProject::pay(...)', function () {
       deployer,
       addrs,
       mockJbDirectory,
+      mockJbTerminal,
       jbFakeProject,
     };
   }
 
   it(`Should pay funds towards project`, async function () {
-    // TODO(odd-amphora): implement.
+    const { jbFakeProject, mockJbDirectory, mockJbTerminal } = await setup();
+
+    await mockJbDirectory.mock.primaryTerminalOf
+      .withArgs(
+        PROJECT_ID,
+        TOKEN
+      )
+      .returns(mockJbTerminal.address);
+
+    await mockJbTerminal.mock.pay
+      .withArgs(
+        PROJECT_ID,
+        BENEFICIARY,
+        0,
+        PREFER_CLAIMED_TOKENS,
+        MEMO,
+        []
+      ).returns();
+
+    await expect(jbFakeProject.pay(
+      BENEFICIARY,
+      MEMO,
+      PREFER_CLAIMED_TOKENS,
+      TOKEN,
+      {
+        value: ethers.utils.parseEther("1.0")
+      }
+    )).to.not.be.reverted;
   });
 
   it(`Fallback function should pay funds towards project`, async function () {
-    // TODO(odd-amphora): implement.
+    const { jbFakeProject, mockJbDirectory, mockJbTerminal, addrs } = await setup();
+
+    let caller = addrs[0];
+
+    await mockJbDirectory.mock.primaryTerminalOf
+      .withArgs(
+        PROJECT_ID,
+        ethers.constants.AddressZero
+      )
+      .returns(mockJbTerminal.address);
+
+    await mockJbTerminal.mock.pay
+      .withArgs(
+        PROJECT_ID,
+        caller.address,
+        0,
+        /*preferClaimedTokens=*/false,
+        /*memo=*/'',
+        []
+      ).returns();
+
+    await expect(caller.sendTransaction({
+      to: jbFakeProject.address,
+      value: ethers.utils.parseEther("1.0"),
+    })).to.not.be.reverted;
   });
 
   it(`Can't pay if project not found`, async function () {
@@ -61,7 +114,7 @@ describe('JBProject::pay(...)', function () {
 
     await mockJbDirectory.mock.primaryTerminalOf
       .withArgs(
-        INITIAL_PROJECT_ID,
+        PROJECT_ID,
         TOKEN
       )
       .returns(ethers.constants.AddressZero);
