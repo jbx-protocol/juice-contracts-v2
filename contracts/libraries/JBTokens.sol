dc82d3eb9c3f9577a5e5a0316077e6a44fd44353
Exekias <odd_amphora@protonmail.com>
2021-12-14 18:40:04 -0500

    comment

--- contracts/libraries/JBTokens.sol
+++ contracts/libraries/JBTokens.sol
@@ -5,6 +5,10 @@ library JBTokens {
   /** 
     @notice 
     The ETH token address in Juicebox is represented by 0x000000000000000000000000000000000000eeee.
+
+    @dev
+    This address is guaranteed to never conflict with other tokens per the following:
+    https://github.com/ethereum/EIPs/pull/1352/files#diff-02f3b07abd45fe04d908b93f8b7aa6d7
   */
   address public constant ETH = address(0xeeee);
 }
