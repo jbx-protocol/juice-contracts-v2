f6d0db5f13163da1c7912596bb377b6e272c02ab
drgorillamd <dr_gorilla@protonmail.com>
2021-12-15 13:16:30 +0100

    wip

--- test/jb_controller/change_token_of.js
+++ test/jb_controller/change_token_of.js
@@ -5,8 +5,8 @@ import { deployMockContract } from '@ethereum-waffle/mock-contract';
 import jbOperatoreStore from '../../artifacts/contracts/JBOperatorStore.sol/JBOperatorStore.json';
 import jbProjects from '../../artifacts/contracts/JBProjects.sol/JBProjects.json';
 import jbDirectory from '../../artifacts/contracts/JBDirectory.sol/JBDirectory.json';
-import jbFundingCycle from '../../artifacts/contracts/JBFundingCycle.sol/JBFundingCycle.json';
 import jbFundingCycleStore from '../../artifacts/contracts/JBFundingCycleStore.sol/JBFundingCycleStore.json';
+import iJbFundingCycleDataSource from '../../artifacts/contracts/interfaces/IJBFundingCycleDataSource.sol/IJBFundingCycleDataSource.json';
 import jbTokenStore from '../../artifacts/contracts/JBTokenStore.sol/JBTokenStore.json';
 import jbSplitsStore from '../../artifacts/contracts/JBSplitsStore.sol/JBSplitsStore.json';
 import jbToken from '../../artifacts/contracts/JBToken.sol/JBToken.json';
@@ -32,6 +32,7 @@ describe('JBController::changeTokenOf(...)', function () {
     let mockJbProjects = await deployMockContract(deployer, jbProjects.abi);
     let mockJbDirectory = await deployMockContract(deployer, jbDirectory.abi);
     let mockJbFundingCycleStore = await deployMockContract(deployer, jbFundingCycleStore.abi);
+    let mockJbFundingCycleDataSource = await deployMockContract(deployer, iJbFundingCycleDataSource);
     let mockSplitsStore = await deployMockContract(deployer, jbSplitsStore.abi);
     let mockTokenStore = await deployMockContract(deployer, jbTokenStore.abi);
     let mockToken = await deployMockContract(deployer, jbToken.abi);
@@ -68,13 +69,14 @@ describe('JBController::changeTokenOf(...)', function () {
   it(`Should change current token if caller is project owner and funding cycle not paused`, async function () {
     const { projectOwner, addrs, jbController, mockToken, mockJbFundingCycleStore } = await setup();
     let newTokenOwner = addrs[O];
-    let mockFundingCycle = await deployMockContract(deployer, jbFundingCycle.abi);
+
+
 
     await mockJbFundingCycleStore.mock.currentOf
       .withArgs(PROJECT_ID)
       .returns(mockFundingCycle.address);
 
-    await mockFundingCycle.mock.changeTokenAllowed
+    await mockJbFundingCycleStore.mock.changeTokenAllowed
       .returns(true);
 
     await mockTokenStore.mock.changeFor
