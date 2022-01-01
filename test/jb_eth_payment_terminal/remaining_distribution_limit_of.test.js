497a2bd60f2a8b5e2e2083050429cfda6617572c
DrGorilla_md <dr_gorilla@protonmail.com>
2022-01-01 15:05:47 +0100

    cuz I fell in love with the girl at the mock show

--- test/jb_eth_payment_terminal/remaining_distribution_limit_of.test.js
+++ test/jb_eth_payment_terminal/remaining_distribution_limit_of.test.js
@@ -8,13 +8,19 @@ import jbProjects from '../../artifacts/contracts/JBProjects.sol/JBProjects.json
 import jbDirectory from '../../artifacts/contracts/JBDirectory.sol/JBDirectory.json';
 import jbSplitsStore from '../../artifacts/contracts/JBSplitsStore.sol/JBSplitsStore.json';
 import jbEthPaymentTerminalStore from '../../artifacts/contracts/JBETHPaymentTerminalStore.sol/JBETHPaymentTerminalStore.json';
+import jbController from '../../artifacts/contracts/JBController.sol/JBController.json';
 
-describe('JBETHPaymentTerminal::ethBalanceOf(...)', function () {
+describe('JBETHPaymentTerminal::remainingDistributionLimitOf(...)', function () {
   const PROJECT_ID = 13;
+  const FUNDING_CYCLE_NUMBER = 1;
   const BALANCE = 100;
 
   async function setup() {
     let [deployer, terminalOwner, ...addrs] = await ethers.getSigners();
+
+    const blockNum = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNum);
+    const timestamp = block.timestamp;
 
     let promises = [];
     promises.push(deployMockContract(deployer, jbOperatoreStore.abi));
@@ -22,6 +28,7 @@ describe('JBETHPaymentTerminal::ethBalanceOf(...)', function () {
     promises.push(deployMockContract(deployer, jbDirectory.abi));
     promises.push(deployMockContract(deployer, jbSplitsStore.abi));
     promises.push(deployMockContract(deployer, jbEthPaymentTerminalStore.abi));
+    promises.push(deployMockContract(deployer, jbController.abi));
 
     let [
       mockJbOperatorStore,
@@ -29,6 +36,7 @@ describe('JBETHPaymentTerminal::ethBalanceOf(...)', function () {
       mockJbDirectory,
       mockSplitsStore,
       mockJbEthPaymentTerminalStore,
+      mockJbController,
     ] = await Promise.all(promises);
 
     let jbTerminalFactory = await ethers.getContractFactory("JBETHPaymentTerminal", deployer);
@@ -52,17 +60,34 @@ describe('JBETHPaymentTerminal::ethBalanceOf(...)', function () {
       terminalOwner,
       addrs,
       jbEthPaymentTerminal,
-      mockJbEthPaymentTerminalStore
+      mockJbDirectory,
+      mockJbEthPaymentTerminalStore,
+      mockJbController,
+      timestamp
     }
   }
 
-  it('Should return the balance of the project', async function () {
-    const { jbEthPaymentTerminal, mockJbEthPaymentTerminalStore } = await setup();
+  it('Should return the remaining distribution limit of the project', async function () {
+    const { deployer, jbEthPaymentTerminal, mockJbDirectory, mockJbController, mockJbEthPaymentTerminalStore, timestamp } = await setup();
 
-    await mockJbEthPaymentTerminalStore.mock.balanceOf
+    await mockJbDirectory.mock.controllerOf
       .withArgs(PROJECT_ID)
+      .returns(mockJbController.address);
+
+    await mockJbController.mock.distributionLimitOf
+      .withArgs(PROJECT_ID, timestamp, jbEthPaymentTerminal.address)
+      .returns(BALANCE);
+
+    await mockJbEthPaymentTerminalStore.mock.usedDistributionLimitOf
+      .withArgs(PROJECT_ID, FUNDING_CYCLE_NUMBER)
       .returns(BALANCE)
 
-    expect(await jbEthPaymentTerminal.ethBalanceOf(PROJECT_ID)).to.equal(BALANCE);
+    expect(
+      await jbEthPaymentTerminal.remainingDistributionLimitOf(
+        PROJECT_ID,
+        timestamp,
+        FUNDING_CYCLE_NUMBER
+      )
+    ).to.equal(0);
   });
 });
