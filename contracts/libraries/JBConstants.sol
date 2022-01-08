9f6711f4c500bf32b048f0a1003a264192ab6736
Exekias <odd_amphora@protonmail.com>
2022-01-08 10:13:08 -0500

    Format

--- contracts/libraries/JBConstants.sol
+++ contracts/libraries/JBConstants.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.6;
 
 /**
-@notice
-Global constants used across multiple Juicebox contracts.
+  @notice
+  Global constants used across multiple Juicebox contracts.
 */
 library JBConstants {
   /** 
