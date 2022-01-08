1525df7ac41f5709e1918448c487508b53bc6595
Exekias <odd_amphora@protonmail.com>
2022-01-08 10:11:57 -0500

    Discount rate

--- contracts/libraries/JBConstants.sol
+++ contracts/libraries/JBConstants.sol
@@ -26,6 +26,12 @@ library JBConstants {
 
   /** 
     @notice
+    A funding cycle's discount rate is expressed as a percentage out of 100000000.
+  */
+  uint256 public constant MAX_DISCOUNT_RATE = 100000000;
+
+  /** 
+    @notice
     Maximum splits percentage.
   */
   uint256 public constant SPLITS_TOTAL_PERCENT = 10000000;
