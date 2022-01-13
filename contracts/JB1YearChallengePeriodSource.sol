0b9d3e6ae8ec176a6974cddf7cfc141f964f9e00
Ezra Weller <ezra.weller@gmail.com>
2022-01-13 14:26:21 -0500

    simpler 1 year

--- contracts/JB1YearChallengePeriodSource.sol
+++ contracts/JB1YearChallengePeriodSource.sol
@@ -5,12 +5,6 @@ import './interfaces/IJBChallengePeriodSource.sol';
 
 contract JB1YearChallengePeriodSource is IJBChallengePeriodSource {
   /** 
-    @notice
-    The number of seconds in 365 days.
-  */
-  uint256 private constant _SECONDS_IN_YEAR = 31536000;
-
-  /** 
     @notice Get the total challenge expiry time when challenging an inactive project handle.
 
     @dev The projectId param is unused here, but could be used in other implementations of IJBChallengeExpiry 
@@ -18,6 +12,6 @@ contract JB1YearChallengePeriodSource is IJBChallengePeriodSource {
   */
 
   function getChallengePeriod(uint256) external view override returns (uint256) {
-    return block.timestamp + _SECONDS_IN_YEAR;
+    return block.timestamp + 31536000; // Number of seconds in 365 days
   }
 }
