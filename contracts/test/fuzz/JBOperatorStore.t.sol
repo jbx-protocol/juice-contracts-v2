b468abb278a160e95a360367be291cdfbf1e2751
Tarrence van As <tarrence@vanas.family>
2022-01-19 10:42:44 -0500

    test(JBOperatorStore): add fuzz example

---
+++ contracts/test/fuzz/JBOperatorStore.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.6;
+
+import 'ds-test/test.sol';
+import 'forge-std/Vm.sol';
+
+import {JBOperatorStore, PERMISSION_INDEX_OUT_OF_BOUNDS} from '../../../contracts/JBOperatorStore.sol';
+import {JBOperatorData} from '../../../contracts/structs/JBOperatorData.sol';
+
+contract JBOperatorStoreTest is DSTest {
+  Vm public constant vm = Vm(HEVM_ADDRESS);
+
+  JBOperatorStore internal store;
+
+  function setUp() public {
+    store = new JBOperatorStore();
+  }
+
+  function testSetOperator(uint256 domain, uint8[64] calldata _idxs) public {
+    address operator = vm.addr(1);
+    uint256[] memory idxs = new uint256[](_idxs.length);
+    for (uint256 i = 0; i < _idxs.length; i++) {
+      idxs[i] = _idxs[i];
+    }
+    store.setOperator(
+      JBOperatorData({operator: operator, domain: domain, permissionIndexes: idxs})
+    );
+    assertTrue(store.hasPermissions(operator, address(this), domain, idxs));
+  }
+
+  function testSetOperators(uint256[10] calldata domains, uint8[64] calldata _idxs) public {
+    address operator = vm.addr(1);
+
+    uint256[] memory idxs = new uint256[](_idxs.length);
+    for (uint256 i = 0; i < _idxs.length; i++) {
+      idxs[i] = _idxs[i];
+    }
+
+    JBOperatorData[] memory _operatorData = new JBOperatorData[](10);
+    for (uint256 i = 0; i < domains.length; i++) {
+      _operatorData[i] = JBOperatorData({
+        operator: operator,
+        domain: domains[i],
+        permissionIndexes: idxs
+      });
+    }
+
+    store.setOperators(_operatorData);
+
+    for (uint256 i = 0; i < _operatorData.length; i++) {
+      assertTrue(
+        store.hasPermissions(
+          operator,
+          address(this),
+          _operatorData[i].domain,
+          _operatorData[i].permissionIndexes
+        )
+      );
+    }
+  }
+}
