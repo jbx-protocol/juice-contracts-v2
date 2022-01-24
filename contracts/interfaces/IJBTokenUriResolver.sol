d777619b3c74d667416cf98bb9a214221d4e9467
mejango <mejango@protonmail.com>
2022-01-23 18:10:04 -0600

    -

--- contracts/interfaces/IJBTokenUriResolver.sol
+++ contracts/interfaces/IJBTokenUriResolver.sol
@@ -2,9 +2,5 @@
 pragma solidity 0.8.6;
 
 interface IJBTokenUriResolver {
-  event SetUri(uint256 indexed projectId, string oldUri, string newUri);
-
-  function setUri(uint256 projectId, string memory newUri) external returns (bool success);
-
   function getUri(uint256 _projectId) external view returns (string memory tokenUri);
 }
