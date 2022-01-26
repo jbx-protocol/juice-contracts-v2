45476830ce9e7c38d2038a968cfda8dd0f26e504
mejango <mejango@protonmail.com>
2022-01-26 17:25:22 -0600

    send arg as struct

--- contracts/interfaces/IJBSplitAllocator.sol
+++ contracts/interfaces/IJBSplitAllocator.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
+import '../structs/JBSplit.sol';
+
 interface IJBSplitAllocator {
   function allocate(
     uint256 _amount,
-    uint256 _group,
     uint256 _projectId,
-    uint256 _forProjectId,
-    address _beneficiary,
-    bool _preferClaimed
+    uint256 _group,
+    JBSplit calldata _split
   ) external payable;
 }
