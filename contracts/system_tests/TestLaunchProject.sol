5eeb122894caec38dfbdabd6996b272c83ee4e78
DrGorilla_md <dr_gorilla@protonmail.com>
2022-02-02 00:08:40 +0100

    bug: funding cycle number 0 ?

---
+++ contracts/system_tests/TestLaunchProject.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import './helpers/TestBaseWorkflow.sol';
+
+contract TestLaunchProject is TestBaseWorkflow {
+  function testLaunchProject() public {
+    uint256 WEIGHT = 1000 * 10**18;
+    JBController controller = jbController();
+
+    JBProjectMetadata memory _projectMetadata = JBProjectMetadata({
+      content: 'myIPFSHash',
+      domain: 1
+    });
+
+    JBFundingCycleData memory _data = JBFundingCycleData({
+      duration: 14,
+      weight: WEIGHT,
+      discountRate: 450000000,
+      ballot: IJBFundingCycleBallot(address(0))
+    });
+
+    JBFundingCycleMetadata memory _metadata = JBFundingCycleMetadata({
+      reservedRate: 5000,
+      redemptionRate: 5000,
+      ballotRedemptionRate: 0,
+      pausePay: false,
+      pauseDistributions: false,
+      pauseRedeem: false,
+      pauseMint: false,
+      pauseBurn: false,
+      allowChangeToken: false,
+      allowTerminalMigration: false,
+      allowControllerMigration: false,
+      holdFees: false,
+      useLocalBalanceForRedemptions: false,
+      useDataSourceForPay: false,
+      useDataSourceForRedeem: false,
+      dataSource: IJBFundingCycleDataSource(address(0))
+    });
+
+    JBGroupedSplits[] memory _groupedSplits = new JBGroupedSplits[](0);
+
+    JBFundAccessConstraints[] memory _fundAccessConstraints = new JBFundAccessConstraints[](0);
+
+    IJBTerminal[] memory _terminals = new IJBTerminal[](0);
+
+    uint256 projectId = controller.launchProjectFor(
+      msg.sender,
+      _projectMetadata,
+      _data,
+      _metadata,
+      block.timestamp,
+      _groupedSplits,
+      _fundAccessConstraints,
+      _terminals
+    );
+
+    JBFundingCycle memory fundingCycle = jbFundingCycleStore().currentOf(projectId); //, latestConfig);
+
+    emit log_uint(fundingCycle.number);
+    emit log_uint(projectId);
+
+    assertEq(fundingCycle.number, 0); // 0 ?!?
+  }
+}
