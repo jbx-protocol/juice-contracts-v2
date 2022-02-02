e9151f9970d1aba1523c919f09dd97c69692c7a1
drgorillamd <dr_gorilla@protonmail.com>
2022-02-02 13:02:04 +0100

    feat: example fuzz weight launchProject

--- contracts/system_tests/TestLaunchProject.sol
+++ contracts/system_tests/TestLaunchProject.sol
@@ -62,4 +62,77 @@ contract TestLaunchProject is TestBaseWorkflow {
     assertEq(fundingCycle.number, 1);
     assertEq(fundingCycle.weight, WEIGHT);
   }
+
+  function testLaunchProjectFuzzWeight(uint256 WEIGHT) public {
+    JBController controller = jbController();
+
+    JBProjectMetadata memory _projectMetadata = JBProjectMetadata({
+      content: 'myIPFSHash',
+      domain: 1
+    });
+
+    JBFundingCycleData memory _data = JBFundingCycleData({
+      duration: 14,
+      weight: WEIGHT,
+      discountRate: 450000000,
+      ballot: IJBFundingCycleBallot(address(0))
+    });
+
+    JBFundingCycleMetadata memory _metadata = JBFundingCycleMetadata({
+      reservedRate: 5000,
+      redemptionRate: 5000,
+      ballotRedemptionRate: 0,
+      pausePay: false,
+      pauseDistributions: false,
+      pauseRedeem: false,
+      pauseMint: false,
+      pauseBurn: false,
+      allowChangeToken: false,
+      allowTerminalMigration: false,
+      allowControllerMigration: false,
+      holdFees: false,
+      useLocalBalanceForRedemptions: false,
+      useDataSourceForPay: false,
+      useDataSourceForRedeem: false,
+      dataSource: IJBFundingCycleDataSource(address(0))
+    });
+
+    JBGroupedSplits[] memory _groupedSplits = new JBGroupedSplits[](0);
+
+    JBFundAccessConstraints[] memory _fundAccessConstraints = new JBFundAccessConstraints[](0);
+
+    IJBTerminal[] memory _terminals = new IJBTerminal[](0);
+
+    // expectRevert on the next call if weight overflowing
+    if (WEIGHT > type(uint88).max) {
+      evm.expectRevert(abi.encodeWithSignature('INVALID_WEIGHT()'));
+
+      uint256 projectId = controller.launchProjectFor(
+        msg.sender,
+        _projectMetadata,
+        _data,
+        _metadata,
+        block.timestamp,
+        _groupedSplits,
+        _fundAccessConstraints,
+        _terminals
+      );
+    } else {
+      uint256 projectId = controller.launchProjectFor(
+        msg.sender,
+        _projectMetadata,
+        _data,
+        _metadata,
+        block.timestamp,
+        _groupedSplits,
+        _fundAccessConstraints,
+        _terminals
+      );
+
+      JBFundingCycle memory fundingCycle = jbFundingCycleStore().currentOf(projectId); //, latestConfig);
+
+      assertEq(fundingCycle.number, 1);
+      assertEq(fundingCycle.weight, WEIGHT);
+    }
+  }
 }
