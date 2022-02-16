d66abddd99ad648d5dd83928a82888adf5fc48b9
drgorillamd <dr_gorilla@protonmail.com>
2022-02-16 16:28:45 +0100

    add constraints

--- contracts/system_tests/TestAllowance.sol
+++ contracts/system_tests/TestAllowance.sol
@@ -110,13 +110,13 @@ contract TestAllowance is TestBaseWorkflow {
     );
   }
 
-  function testFuzzAllowance(uint248 ALLOWANCE, uint96 BALANCE) public {
+  function testFuzzAllowance(uint248 ALLOWANCE, uint248 TARGET, uint96 BALANCE) public {
     JBETHPaymentTerminal terminal = jbETHPaymentTerminal();
 
     _fundAccessConstraints.push(
       JBFundAccessConstraints({
         terminal: jbETHPaymentTerminal(),
-        distributionLimit: 10 ether,
+        distributionLimit: TARGET,
         overflowAllowance: ALLOWANCE,
         distributionLimitCurrency: 1, // Currency = ETH
         overflowAllowanceCurrency: 1
@@ -152,9 +152,15 @@ contract TestAllowance is TestBaseWorkflow {
     );
 
     evm.prank(_projectOwner);
+    if (TARGET > BALANCE)
+      evm.expectRevert(abi.encodeWithSignature('INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()'));
+    
+    if (TARGET == 0)
+      evm.expectRevert(abi.encodeWithSignature('DISTRIBUTION_AMOUNT_LIMIT_REACHED()'));
+
     terminal.distributePayoutsOf(
       projectId,
-      10 ether,
+      TARGET,
       1, // Currency
       0, // Min wei out
       'Foundry payment' // Memo
@@ -172,68 +178,68 @@ contract TestAllowance is TestBaseWorkflow {
     );
   }
 
-  function testFuzzTarget(uint248 TARGET, uint96 BALANCE) public {
-    JBETHPaymentTerminal terminal = jbETHPaymentTerminal();
+  // function testFuzzTarget(uint248 TARGET, uint96 BALANCE) public {
+  //   JBETHPaymentTerminal terminal = jbETHPaymentTerminal();
 
-    _fundAccessConstraints.push(
-      JBFundAccessConstraints({
-        terminal: jbETHPaymentTerminal(),
-        distributionLimit: TARGET,
-        overflowAllowance: 5 ether,
-        distributionLimitCurrency: 1,
-        overflowAllowanceCurrency: 1
-      })
-    );
+  //   _fundAccessConstraints.push(
+  //     JBFundAccessConstraints({
+  //       terminal: jbETHPaymentTerminal(),
+  //       distributionLimit: TARGET,
+  //       overflowAllowance: 5 ether,
+  //       distributionLimitCurrency: 1,
+  //       overflowAllowanceCurrency: 1
+  //     })
+  //   );
 
-    uint256 projectId = controller.launchProjectFor(
-      _projectOwner,
-      _projectMetadata,
-      _data,
-      _metadata,
-      block.timestamp,
-      _groupedSplits,
-      _fundAccessConstraints,
-      _terminals
-    );
+  //   uint256 projectId = controller.launchProjectFor(
+  //     _projectOwner,
+  //     _projectMetadata,
+  //     _data,
+  //     _metadata,
+  //     block.timestamp,
+  //     _groupedSplits,
+  //     _fundAccessConstraints,
+  //     _terminals
+  //   );
 
-    terminal.pay{value: BALANCE}(projectId, msg.sender, 0, false, 'Forge test', new bytes(0));
+  //   terminal.pay{value: BALANCE}(projectId, msg.sender, 0, false, 'Forge test', new bytes(0));
 
-    evm.prank(_projectOwner);
-    if (TARGET > BALANCE)
-      evm.expectRevert(abi.encodeWithSignature('INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()'));
-    terminal.useAllowanceOf(
-      projectId,
-      5 ether,
-      1, // Currency
-      0, // Min wei out
-      payable(msg.sender) // Beneficiary
-    );
+  //   evm.prank(_projectOwner);
+  //   if (TARGET > BALANCE)
+  //     evm.expectRevert(abi.encodeWithSignature('INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()'));
+  //   terminal.useAllowanceOf(
+  //     projectId,
+  //     5 ether,
+  //     1, // Currency
+  //     0, // Min wei out
+  //     payable(msg.sender) // Beneficiary
+  //   );
 
-    // Distribute the funding target ETH -> no split then beneficiary is the project owner
-    evm.prank(_projectOwner);
-    if (TARGET > BALANCE)
-      evm.expectRevert(abi.encodeWithSignature('INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()'));
-      
-    if (TARGET == 0)
-      evm.expectRevert(abi.encodeWithSignature('DISTRIBUTION_AMOUNT_LIMIT_REACHED()'));
+  //   // Distribute the funding target ETH -> no split then beneficiary is the project owner
+  //   evm.prank(_projectOwner);
+  //   if (TARGET > BALANCE)
+  //     evm.expectRevert(abi.encodeWithSignature('INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()'));
 
-    terminal.distributePayoutsOf(
-      projectId,
-      TARGET,
-      1, // Currency
-      0, // Min wei out
-      'Foundry payment' // Memo
-    );
+  //   if (TARGET == 0)
+  //     evm.expectRevert(abi.encodeWithSignature('DISTRIBUTION_AMOUNT_LIMIT_REACHED()'));
 
-    evm.prank(msg.sender); // Fixing the msg.sender "instability" bug
-    terminal.redeemTokensOf(
-      msg.sender,
-      projectId,
-      1, // Currency
-      0, // Min wei out
-      payable(msg.sender),
-      'gimme my money back',
-      new bytes(0)
-    );
-  }
+  //   terminal.distributePayoutsOf(
+  //     projectId,
+  //     TARGET,
+  //     1, // Currency
+  //     0, // Min wei out
+  //     'Foundry payment' // Memo
+  //   );
+
+  //   evm.prank(msg.sender); // Fixing the msg.sender "instability" bug
+  //   terminal.redeemTokensOf(
+  //     msg.sender,
+  //     projectId,
+  //     1, // Currency
+  //     0, // Min wei out
+  //     payable(msg.sender),
+  //     'gimme my money back',
+  //     new bytes(0)
+  //   );
+  // }
 }
