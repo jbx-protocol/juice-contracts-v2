03f7d571a6c06f57b5b0dbc4351970f3fe116708
jango <77952627+mejango@users.noreply.github.com>
2022-03-20 10:55:56 -0500

    tokenStore.claim requires holder permission (#172)
    * tokenStore.claim requires holder permission
    
    * allow project owner to claim for holders

--- contracts/JBTokenStore.sol
+++ contracts/JBTokenStore.sol
@@ -355,7 +355,7 @@ contract JBTokenStore is IJBTokenStore, JBControllerUtility, JBOperatable {
     Claims internally accounted for tokens into a holder's wallet.
 
     @dev
-    Anyone can claim tokens on behalf of a token owner.
+    Only a token holder, the owner of the token's project, or an operator specified by the token holder can claim its unclaimed tokens.
 
     @param _holder The owner of the tokens being claimed.
     @param _projectId The ID of the project whose tokens are being claimed.
@@ -365,7 +365,16 @@ contract JBTokenStore is IJBTokenStore, JBControllerUtility, JBOperatable {
     address _holder,
     uint256 _projectId,
     uint256 _amount
-  ) external override {
+  )
+    external
+    override
+    requirePermissionAllowingOverride(
+      _holder,
+      _projectId,
+      JBOperations.CLAIM,
+      msg.sender == projects.ownerOf(_projectId)
+    )
+  {
     // Get a reference to the project's current token.
     IJBToken _token = tokenOf[_projectId];
 
