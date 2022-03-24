d6ca4ca8419f79538a743865df56cc4ba3205838
mejango <mejango@protonmail.com>
2022-03-24 14:38:24 -0300

    -

--- contracts/structs/JBTokenAmount.sol
+++ contracts/structs/JBTokenAmount.sol
@@ -8,6 +8,6 @@ struct JBTokenAmount {
   uint256 value;
   // The number of decimals included in the value fixed point number.
   uint256 decimals;
-  // The expected currency index of the value.
+  // The expected currency of the value.
   uint256 currency;
 }
