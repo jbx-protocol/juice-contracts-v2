92aea8e9ccf7b3fffd2bef78f778d7c2ed24b0f0
mejango <mejango@protonmail.com>
2022-03-26 15:51:20 -0300

    adjusted param

--- contracts/structs/JBRedeemParamsData.sol
+++ contracts/structs/JBRedeemParamsData.sol
@@ -14,14 +14,14 @@ struct JBRedeemParamsData {
   uint256 tokenCount;
   // The total supply of tokens used in the calculation, as a fixed point number with 18 decimals.
   uint256 totalSupply;
+  // The amount of overflow used in the reclaim amount calculation.
+  uint256 overflow;
   // The number of decimals included in the reclaim amount fixed point number.
   uint256 decimals;
   // The currency that the reclaim amount is expected to be in terms of.
   uint256 currency;
   // The amount that should be reclaimed by the redeemer using the protocol's standard bonding curve redemption formula.
   uint256 reclaimAmount;
-  // The amount of overflow used in the reclaim amount calculation.
-  uint256 overflow;
   // If overflow across all of a project's terminals is being used when making redemptions.
   bool useTotalOverflow;
   // The redemption rate of the funding cycle during which the redemption is being made.
