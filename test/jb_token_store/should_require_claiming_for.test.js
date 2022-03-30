d5c17176edb01f0e1fee944f414b93be9da4952b
DrGorilla_md <dr_gorilla@protonmail.com>
2022-03-30 23:43:55 +0200

    auth wasn't really tested (but worked)

--- test/jb_token_store/should_require_claiming_for.test.js
+++ test/jb_token_store/should_require_claiming_for.test.js
@@ -8,13 +8,13 @@ import jbOperatoreStore from '../../artifacts/contracts/JBOperatorStore.sol/JBOp
 import jbProjects from '../../artifacts/contracts/JBProjects.sol/JBProjects.json';
 import errors from '../helpers/errors.json';
 
-describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
+describe.only('JBTokenStore::shouldRequireClaimingFor(...)', function () {
   const PROJECT_ID = 2;
   const TOKEN_NAME = 'TestTokenDAO';
   const TOKEN_SYMBOL = 'TEST';
 
   async function setup() {
-    const [deployer, controller, holder, recipient] = await ethers.getSigners();
+    const [deployer, controller, projectOwner, holder, recipient] = await ethers.getSigners();
 
     const jbOperationsFactory = await ethers.getContractFactory('JBOperations');
     const jbOperations = await jbOperationsFactory.deploy();
@@ -34,6 +34,7 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
 
     return {
       controller,
+      projectOwner,
       holder,
       recipient,
       mockJbDirectory,
@@ -47,16 +48,14 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
   it('Should set flag and emit event if caller is project owner', async function () {
     const {
       controller,
-      holder,
+      projectOwner,
       mockJbDirectory,
-      mockJbOperatorStore,
       mockJbProjects,
       jbTokenStore,
-      REQUIRE_CLAIM_INDEX,
     } = await setup();
 
     await mockJbDirectory.mock.controllerOf.withArgs(PROJECT_ID).returns(controller.address);
-    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(controller.address);
+    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(projectOwner.address);
 
     // Issue token for project
     await jbTokenStore.connect(controller).issueFor(PROJECT_ID, TOKEN_NAME, TOKEN_SYMBOL);
@@ -64,19 +63,20 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
     // Set flag value
     const flagVal = true;
     const shouldRequireClaimingForTx = await jbTokenStore
-      .connect(controller)
+      .connect(projectOwner)
       .shouldRequireClaimingFor(PROJECT_ID, flagVal);
 
     expect(await jbTokenStore.requireClaimFor(PROJECT_ID)).to.equal(flagVal);
 
     await expect(shouldRequireClaimingForTx)
       .to.emit(jbTokenStore, 'ShouldRequireClaim')
-      .withArgs(PROJECT_ID, flagVal, controller.address);
+      .withArgs(PROJECT_ID, flagVal, projectOwner.address);
   });
 
   it('Should set flag and emit event if caller has permission', async function () {
     const {
       controller,
+      projectOwner,
       holder,
       mockJbDirectory,
       mockJbOperatorStore,
@@ -86,9 +86,10 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
     } = await setup();
 
     await mockJbDirectory.mock.controllerOf.withArgs(PROJECT_ID).returns(controller.address);
-    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(controller.address);
+
+    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(projectOwner.address);
     await mockJbOperatorStore.mock.hasPermission
-      .withArgs(holder.address, controller.address, PROJECT_ID, REQUIRE_CLAIM_INDEX)
+      .withArgs(holder.address, projectOwner.address, PROJECT_ID, REQUIRE_CLAIM_INDEX)
       .returns(true);
 
     // Issue token for project
@@ -129,7 +130,7 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
 
   it(`Can't set flag if caller lacks permission`, async function () {
     const {
-      controller,
+      projectOwner,
       holder,
       mockJbOperatorStore,
       mockJbProjects,
@@ -137,13 +138,17 @@ describe('JBTokenStore::shouldRequireClaimingFor(...)', function () {
       REQUIRE_CLAIM_INDEX,
     } = await setup();
 
-    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(controller.address);
+    await mockJbProjects.mock.ownerOf.withArgs(PROJECT_ID).returns(projectOwner.address);
     await mockJbOperatorStore.mock.hasPermission
-      .withArgs(controller.address, holder.address, PROJECT_ID, REQUIRE_CLAIM_INDEX)
+      .withArgs(holder.address, projectOwner.address, PROJECT_ID, REQUIRE_CLAIM_INDEX)
+      .returns(false);
+
+    await mockJbOperatorStore.mock.hasPermission
+      .withArgs(holder.address, projectOwner.address, 0, REQUIRE_CLAIM_INDEX)
       .returns(false);
 
     await expect(
-      jbTokenStore.connect(controller).shouldRequireClaimingFor(PROJECT_ID, /* flag= */ false),
-    ).to.be.reverted;
+      jbTokenStore.connect(holder).shouldRequireClaimingFor(PROJECT_ID, /* flag= */ false),
+    ).to.be.revertedWith(errors.UNAUTHORIZED);
   });
 });
