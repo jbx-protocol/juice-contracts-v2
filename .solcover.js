7b78430f5886171cf6ea7519123872ff686baaea
DrGorilla_md <dr_gorilla@protonmail.com>
2022-04-06 12:21:02 +0200

    REALLY?!?

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,17 @@
 // https://www.npmjs.com/package/solidity-coverage
 
 module.exports = {
-  skipFiles: ['system_tests/*.sol'],
+  skipFiles: [
+'system_tests/helpers/AccessJBLib.sol',
+'system_tests/helpers/hevm.sol',
+'system_tests/helpers/TestBaseWorkflow.sol',
+'system_tests/mock/MockPriceFeed.sol',
+'system_tests/TestAllowance.sol',
+'system_tests/TestERC20Terminal.sol',
+'system_tests/TestLaunchProject.sol',
+'system_tests/TestMultipleTerminals.sol',
+'system_tests/TestPayBurnRedeemFlow.sol',
+'system_tests/TestTokenFlow.sol'
+  ],
   configureYulOptimizer: true,
-  measureStatementCoverage: false
 };
