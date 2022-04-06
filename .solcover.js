64a5e928f1c3de61a85d705c208a60bddb088bf2
DrGorilla_md <dr_gorilla@protonmail.com>
2022-04-06 12:14:39 +0200

    .

--- .solcover.js
+++ .solcover.js
@@ -2,5 +2,7 @@
 //
 // https://www.npmjs.com/package/solidity-coverage
 
-export const configureYulOptimizer = true;
-export const skipFiles = ['system_tests/*'];
+module.exports = {
+  skipFiles: ['system_tests/*'],
+  configureYulOptimizer: true
+};
