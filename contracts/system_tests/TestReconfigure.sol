52a54867caa60186cc9ace361306a255fa1eb5a9
DrGorilla_md <dr_gorilla@protonmail.com>
2022-04-07 00:19:35 +0200

    this weird

--- contracts/system_tests/TestReconfigure.sol
+++ contracts/system_tests/TestReconfigure.sol
@@ -35,13 +35,15 @@ contract TestReconfigureProject is TestBaseWorkflow {
       pausePay: false,
       pauseDistributions: false,
       pauseRedeem: false,
-      pauseMint: false,
       pauseBurn: false,
+      allowMinting: true,
       allowChangeToken: false,
       allowTerminalMigration: false,
       allowControllerMigration: false,
+      allowSetTerminals: false,
+      allowSetController: false,
       holdFees: false,
-      useLocalBalanceForRedemptions: false,
+      useTotalOverflowForRedemptions: false,
       useDataSourceForPay: false,
       useDataSourceForRedeem: false,
       dataSource: IJBFundingCycleDataSource(address(0))
@@ -86,11 +88,11 @@ contract TestReconfigureProject is TestBaseWorkflow {
   }
 
   function testReconfigureProjectFuzzRates(
-    uint256 RESERVED_RATE,
-    uint256 REDEMPTION_RATE,
+    uint96 RESERVED_RATE,
+    uint96 REDEMPTION_RATE,
     uint96 BALANCE
   ) public {
-    evm.assume(payable(msg.sender).balance >= BALANCE);
+    evm.assume(payable(msg.sender).balance/2 >= BALANCE);
 
     address _beneficiary = address(69420);
     uint256 projectId = controller.launchProjectFor(
@@ -126,13 +128,15 @@ contract TestReconfigureProject is TestBaseWorkflow {
         pausePay: false,
         pauseDistributions: false,
         pauseRedeem: false,
-        pauseMint: false,
         pauseBurn: false,
+        allowMinting: true,
         allowChangeToken: false,
         allowTerminalMigration: false,
         allowControllerMigration: false,
+        allowSetTerminals: false,
+        allowSetController: false,
         holdFees: false,
-        useLocalBalanceForRedemptions: false,
+        useTotalOverflowForRedemptions: false,
         useDataSourceForPay: false,
         useDataSourceForRedeem: false,
         dataSource: IJBFundingCycleDataSource(address(0))
@@ -143,6 +147,11 @@ contract TestReconfigureProject is TestBaseWorkflow {
       ''
     );
 
+    if (RESERVED_RATE > 10000 || REDEMPTION_RATE > 10000) {
+      REDEMPTION_RATE = 5000; // If reconfigure has reverted, keep previous rates
+      RESERVED_RATE = 5000;
+    }
+
     evm.warp(block.timestamp + 15);
 
     JBFundingCycle memory newFundingCycle = jbFundingCycleStore().currentOf(projectId);
@@ -152,17 +161,19 @@ contract TestReconfigureProject is TestBaseWorkflow {
 
     uint256 _newUserTokenBalance = RESERVED_RATE == 0 // New fc, rate is RESERVED_RATE
       ? PRBMath.mulDiv(BALANCE, WEIGHT, 10**18)
-      : PRBMath.mulDiv(BALANCE, (WEIGHT/10**18), RESERVED_RATE);
-      
+      : PRBMath.mulDiv( PRBMath.mulDiv(BALANCE, WEIGHT, 10**18), 10000-RESERVED_RATE, 10000);
+
     if(BALANCE != 0) assertEq(jbTokenStore().balanceOf(_beneficiary, projectId), _userTokenBalance + _newUserTokenBalance);
 
     uint256 tokenBalance = jbTokenStore().balanceOf(_beneficiary, projectId);
+    uint256 totalSupply = jbTokenStore().totalSupplyOf(projectId);
+    uint256 ethBalance = jbPaymentTerminalStore().balanceOf(jbETHPaymentTerminal(), projectId);
 
     evm.startPrank(_beneficiary);
     jbETHPaymentTerminal().redeemTokensOf(
       _beneficiary,
       projectId,
-      tokenBalance / 2,
+      tokenBalance,
       0,
       payable(_beneficiary),
       '',
@@ -170,7 +181,14 @@ contract TestReconfigureProject is TestBaseWorkflow {
     );
     evm.stopPrank();
 
-    // No fund access constraint -> the whole balance is in overflow and can be redeemed
-    assertEq(_beneficiary.balance,  ( ((tokenBalance/2) / (WEIGHT/10**18)) * REDEMPTION_RATE) / 10000);
+    if(BALANCE != 0)
+      assertEq(
+        _beneficiary.balance,
+        PRBMath.mulDiv(
+          PRBMath.mulDiv(ethBalance, tokenBalance, totalSupply), // Part of the overflow owned (no constraint->everything is overflow)
+          REDEMPTION_RATE + PRBMath.mulDiv(tokenBalance, 10000-REDEMPTION_RATE, totalSupply),
+          10000
+        )
+      );
   }
 }
