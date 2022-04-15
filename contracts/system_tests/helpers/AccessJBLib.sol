30c09c8b0e96c51733b12f0aafe84b2dfc779fa1
DrGorilla_md <dr_gorilla@protonmail.com>
2022-04-15 21:29:31 +0200

    unused var

--- contracts/system_tests/helpers/AccessJBLib.sol
+++ contracts/system_tests/helpers/AccessJBLib.sol
@@ -6,20 +6,20 @@ import '../../libraries/JBConstants.sol';
 import '../../libraries/JBTokens.sol';
 
 contract AccessJBLib {
-    function ETH() external returns(uint256) {
+    function ETH() external pure returns(uint256) {
         return JBCurrencies.ETH;
     }
-    function USD() external returns(uint256) {
+    function USD() external pure returns(uint256) {
         return JBCurrencies.USD;
     }
-    function ETHToken() external returns(address) {
+    function ETHToken() external pure returns(address) {
         return JBTokens.ETH;
     }
-    function MAX_FEE() external returns(uint256) {
+    function MAX_FEE() external pure returns(uint256) {
         return JBConstants.MAX_FEE;
     }
 
-    function SPLITS_TOTAL_PERCENT() external returns(uint256) {
+    function SPLITS_TOTAL_PERCENT() external pure returns(uint256) {
         return JBConstants.SPLITS_TOTAL_PERCENT;
     }
 }
