2b217e0fa5bf3c52ccffbb28defa2f9e1e846a98
DrGorilla_md <dr_gorilla@protonmail.com>
2022-05-29 23:25:07 +0200

    get line 501 too

--- contracts/JBETHERC20SplitsPayer.sol
+++ contracts/JBETHERC20SplitsPayer.sol
@@ -498,7 +498,7 @@ contract JBETHERC20SplitsPayer is IJBSplitsPayer, JBETHERC20ProjectPayer, Reentr
           // Transfer the ETH.
           if (_token == JBTokens.ETH)
             Address.sendValue(
-              // Get a reference to the address receiving the tokens. If there's a beneficiary, send the funds directly to the beneficiary. Otherwise send to the msg.sender.
+              // Get a reference to the address receiving the tokens. If there's a beneficiary, send the funds directly to the beneficiary. Otherwise send to _defaultBeneficiary.
               _split.beneficiary != address(0) ? _split.beneficiary : payable(_defaultBeneficiary),
               _splitAmount
             );
