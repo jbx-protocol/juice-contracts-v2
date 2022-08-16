8d5ec8e8f15caae3a5a3a6796ecb83cc13259ae7
DrGorilla_md <dr_gorilla@protonmail.com>
2022-08-16 14:59:09 +0200

    jbconstants readability

--- contracts/libraries/JBConstants.sol
+++ contracts/libraries/JBConstants.sol
@@ -6,10 +6,10 @@ pragma solidity 0.8.6;
   Global constants used across Juicebox contracts.
 */
 library JBConstants {
-  uint256 public constant MAX_RESERVED_RATE = 10000;
-  uint256 public constant MAX_REDEMPTION_RATE = 10000;
-  uint256 public constant MAX_DISCOUNT_RATE = 1000000000;
-  uint256 public constant SPLITS_TOTAL_PERCENT = 1000000000;
-  uint256 public constant MAX_FEE = 1000000000;
-  uint256 public constant MAX_FEE_DISCOUNT = 1000000000;
+  uint256 public constant MAX_RESERVED_RATE = 10_000;
+  uint256 public constant MAX_REDEMPTION_RATE = 10_000;
+  uint256 public constant MAX_DISCOUNT_RATE = 1_000_000_000;
+  uint256 public constant SPLITS_TOTAL_PERCENT = 1_000_000_000;
+  uint256 public constant MAX_FEE = 1_000_000_000;
+  uint256 public constant MAX_FEE_DISCOUNT = 1_000_000_000;
 }
