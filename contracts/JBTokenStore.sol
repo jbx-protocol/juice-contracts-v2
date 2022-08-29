2fb1382b7e93e2005af053e67854c9fb694bcdc2
mejango <mejango@protonmail.com>
2022-08-29 14:25:04 -0400

    removed check on set

--- contracts/JBTokenStore.sol
+++ contracts/JBTokenStore.sol
@@ -41,7 +41,6 @@ contract JBTokenStore is IJBTokenStore, JBControllerUtility, JBOperatable {
   error INSUFFICIENT_FUNDS();
   error INSUFFICIENT_UNCLAIMED_TOKENS();
   error PROJECT_ALREADY_HAS_TOKEN();
-  error PROJECT_ID_MISMATCH();
   error RECIPIENT_ZERO_ADDRESS();
   error TOKEN_NOT_FOUND();
   error TOKENS_MUST_HAVE_18_DECIMALS();
@@ -234,12 +233,6 @@ contract JBTokenStore is IJBTokenStore, JBControllerUtility, JBOperatable {
   {
     // Can't set to the zero address.
     if (_token == IJBToken(address(0))) revert EMPTY_TOKEN();
-
-    // Get a reference to the ID of the project the token can be used for.
-    uint256 _tokenProjectId = _token.projectId();
-
-    // Can't use the token if the project IDs don't match, and if the token's project ID isn't the wildcard ID.
-    if (_tokenProjectId != _projectId && _tokenProjectId != 0) revert PROJECT_ID_MISMATCH();
 
     // Can't set token if already set.
     if (tokenOf[_projectId] != IJBToken(address(0))) revert ALREADY_SET();
